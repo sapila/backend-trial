3745a9046f1fc183ee2ed9972c41073992b9f84c
sapila <kadamidis@isosoft.gr>
2016-03-15 14:31:56 +0200

    functionality implemented

--- assignment1/index.php
+++ assignment1/index.php
@@ -11,7 +11,7 @@ $commission = 0.10; // 10% commission
 // Prepare query
 $result = $db
 	->prepare('
-		SELECT * FROM bookings
+		SELECT * FROM bookers limit 10000
 	')
 	->run()
 ;
@@ -52,21 +52,146 @@ $result = $db
 					<th>LTV</th>
 				</tr>
 			</thead>
-			<tbody>
-				<?php foreach ($result as $index => $row): ?>
-					<tr>
-						<td>TODO</td>
-						<td>TODO</td>
-						<td>TODO</td>
-						<td>TODO</td>
-						<td>TODO</td>
-					</tr>
-				<?php endforeach; ?>
+			<tbody>   
+                 <?php 
+        class Booker {
+           public $first_booking;
+           public $turnover_sum = 0;
+           public $bookitems_count = 0;
+        }
+        $bookers =array();
+         ?>  
+		
+
+        <?php  $resultBookers = $db
+            //get all booking items for each booker
+            ->prepare('
+                SELECT bookers.id,bookingitems.end_timestamp,bookingitems.locked_total_price FROM bookers 
+                  INNER JOIN bookings ON (bookers.id=bookings.booker_id) 
+                  INNER JOIN bookingitems ON (bookings.id=bookingitems.booking_id)
+                  ORDER BY bookingitems.end_timestamp  
+            ')
+            ->run();
+          
+            
+            $count=0;$turnover=0;$bookings_sum=0;
+            
+            $tempid ;$end_date;
+            $first = true;
+            $booker = new Booker();
+            $rows=0;
+            foreach ($resultBookers as $bookersIndex => $bookersRow):
+
+              if($bookersIndex == 0)
+              {
+                $tempid = $bookersRow->id;
+              }
+
+             
+
+              if($tempid == $bookersRow->id)
+              {
+                if($first == true){
+                //save date of first booking
+                  $booker->first_booking = $bookersRow->end_timestamp;
+                  $end_date = strtotime("+".$period." month", $booker->first_booking);
+                  // echo date('m Y',$booker->first_booking)." date  ".date('m Y',$end_date)." ".$tempid."<br>";
+                  $first = false;
+                }
+              //if booking item is between the period
+              if($bookersRow->end_timestamp <= $end_date)
+               {
+                   $booker->turnover_sum += $bookersRow->locked_total_price;
+                   $booker->bookitems_count++;
+                } 
+                //echo $bookersRow->id.' '.date('m Y',$bookersRow->end_timestamp).' '.$bookersRow->locked_total_price.'<br>'; 
+            }else{
+
+              //new booker id
+               if($booker->bookitems_count>0)
+                $bookers[] = $booker;
+               $booker = new Booker();
+
+              $tempid = $bookersRow->id;
+              
+                //save date of first booking
+                $booker->first_booking = $bookersRow->end_timestamp;
+                        $end_date = strtotime("+".$period." month", $booker->first_booking);
+                       // echo date('m Y',$booker->first_booking)." date  ".date('m Y',$end_date)."<br>";
+                       
+               
+              //if booking item is between the period
+              if($bookersRow->end_timestamp <= $end_date)
+               {
+                   $booker->turnover_sum += $bookersRow->locked_total_price;
+                   $booker->bookitems_count++;
+                } 
+            }
+
+          
+           
+
+             $rows++;
+             // echo 'Results : '.$booker->bookitems_count.' count : '.$booker->turnover_sum.' sum date:'.date('m Y',$booker->first_booking).'<br>';
+            endforeach;
+              
+           
+
+           
+        ?>
+    
+ 
+					
+				<?php 
+                $i;$mindate=$bookers[0]->first_booking;$maxdate=$bookers[0]->first_booking;
+                //find min/max dates of bookings
+                for($i=0;$i<sizeof($bookers);$i++)
+                {
+                    if($bookers[$i]->first_booking<$mindate)
+                        $mindate = $bookers[$i]->first_booking;
+                    if($bookers[$i]->first_booking>$mindate)
+                        $maxdate = $bookers[$i]->first_booking;
+                        
+                } 
+
+                //for every month between min/max date of bookingitems
+                $count=0;$turnover=0;$bookings_sum=0;
+                while($mindate<=$maxdate)
+                {
+                      for($i=0;$i<sizeof($bookers);$i++)
+                        {
+                            //sum data for every bookingitem of the month
+                            if(date('m Y',$bookers[$i]->first_booking)==date('m Y',$mindate))
+                              {
+                                  $bookings_sum+=$bookers[$i]->bookitems_count;
+                                  $turnover+=$bookers[$i]->turnover_sum;
+                                  $count++;
+                              }       
+                        } ?>
+                        
+                    <tr>
+          						<td><?php echo date('m Y',$mindate) ?></td>
+          						<td><?php echo $count ?></td>
+          						<td><?php echo ($bookings_sum/$period)?></td>
+          						<td><?php echo ($turnover/$period)?></td>
+          						<td><?php echo ($turnover/$period)*$commission?></td>
+                                  
+          					</tr>
+                    
+                  <?php
+
+                      $count=0;$turnover=0;$bookings_sum=0;
+                      //go to next month
+                      $mindate = strtotime("+1 month", $mindate);
+                }
+                
+                ?>
+       
 			</tbody>
 			<tfoot>
 				<tr>
 					<td colspan="4" class="right"><strong>Total rows:</strong></td>
-					<td><?= $index + 1 ?></td>
+					<td><?php echo $rows; ?></td>
 				</tr>
 			</tfoot>
 		</table>
